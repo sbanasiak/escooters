6b13b540769b54e98b3e3bf68af537e5d679194a
sbanasiak <seweryn.banasiak@studenci.collegiumwitelona.pl>
2023-03-29 19:34:16 +0200

    Extended MapboxGeocodingService

--- src/Services/MapboxGeocodingService.php
+++ src/Services/MapboxGeocodingService.php
@@ -11,50 +11,155 @@ use GuzzleHttp\Exception\GuzzleException;
 
 class MapboxGeocodingService
 {
-    protected const CACHE_FILENAME = "./.mapbox_cities_cache";
+    protected const CACHE_FILENAMES = [
+        'city' => "./.mapbox_city_cache",
+        'country' => "./.mapbox_country_cache",
+        'coordinates' => "./.mapbox_coordinates_cache",
+    ];
 
-    protected array $cache = [];
+    protected array $cache = [
+        'city' => [],
+        'country' => [],
+        'coordinates' => [],
+    ];
 
     public function __construct(
         protected string $token,
     ) {
-        if (file_exists(static::CACHE_FILENAME)) {
-            echo "Cached cities loaded." . PHP_EOL;
-            $this->cache = json_decode(file_get_contents(static::CACHE_FILENAME), true);
+        foreach (self::CACHE_FILENAMES as $type => $filename) {
+            if (file_exists($filename)) {
+                echo "Cache {$type} loaded." . PHP_EOL;
+                $this->cache[$type] = json_decode(file_get_contents($filename), true);
+            }
         }
     }
 
     public function setCoordinatesToCities(Cities $cities): static
     {
-        $client = new Client();
-
         foreach ($cities->all() as $city) {
-            if (array_key_exists($city->getId(), $this->cache)) {
-                $city->setCoordinates($this->cache[$city->getId()]);
-            } else {
-                $this->getFromAPI($client, $city);
+            $cityId = $city->getId();
+            $coordinates = $this->getFromCacheOrCreate(
+                'city',
+                $cityId,
+                fn() => $this->getCityFromAPI($city)
+            );
+            if ($coordinates !== null) {
+                $city->setCoordinates($coordinates);
             }
         }
 
         return $this;
     }
 
-    protected function getFromAPI(Client $client, City $city): void
+    protected function getCityFromAPI(City $city): ?array
     {
+        $client = new Client();
         $name = $city->getName() . ", " . $city->getCountry()->getName();
-        $token = $this->token;
 
         try {
             $response = $client->get(
-                "https://api.mapbox.com/geocoding/v5/mapbox.places/{$name}.json?access_token={$token}&types=place",
+                "https://api.mapbox.com/geocoding/v5/mapbox.places/{$name}.json?access_token={$this->token}&types=place",
             );
             $coordinates = json_decode($response->getBody()->getContents(), true)["features"][0]["center"];
-            $this->cache[$city->getId()] = $coordinates;
-            $city->setCoordinates($coordinates);
+            $this->updateCache('city', $city->getId(), $coordinates);
+            return $coordinates;
         } catch (GuzzleException) {
-            echo "Coordinates for {$name} were not fetched.";
+            echo "Coordinates for {$name} were not fetched." . PHP_EOL;
+            return null;
+        }
+    }
+
+    public function normalizeCountryName(string $unnormalizedName): ?string
+    {
+        return $this->getFromCacheOrCreate(
+            'country',
+            $unnormalizedName,
+            fn() => $this->getCountryFromAPI($unnormalizedName)
+        );
+    }
+
+    protected function getCountryFromAPI(string $unnormalizedName): ?string
+    {
+        $client = new Client();
+
+        try {
+            $response = $client->get(
+                "https://api.mapbox.com/geocoding/v5/mapbox.places/{$unnormalizedName}.json?access_token={$this->token}&types=country&limit=1&autocomplete=true",
+            );
+
+            $responseArray = json_decode($response->getBody()->getContents(), true);
+
+            if (empty($responseArray["features"])) {
+                echo "Country name for unnormalized name {$unnormalizedName} was not found." . PHP_EOL;
+                return null;
+            }
+
+            $normalizedCountry = $responseArray["features"][0]["text"];
+            $this->updateCache('country', $unnormalizedName, $normalizedCountry);
+
+            return $normalizedCountry;
+        } catch (GuzzleException) {
+            echo "Normalized country name for {$unnormalizedName} was not fetched." . PHP_EOL;
+            return null;
+        }
+    }
+
+    public function getCityNameFromCoordinates(float $latitude, float $longitude): ?string
+    {
+        $coordinatesKey = "{$latitude},{$longitude}";
+
+        return $this->getFromCacheOrCreate(
+            'coordinates',
+            $coordinatesKey,
+            fn() => $this->getCityNameFromAPI($latitude, $longitude)
+        );
+    }
+
+    protected function getCityNameFromAPI(float $latitude, float $longitude): ?string
+    {
+        $client = new Client();
+
+        try {
+            $response = $client->get(
+                "https://api.mapbox.com/geocoding/v5/mapbox.places/{$longitude},{$latitude}.json?access_token={$this->token}&types=place",
+            );
+
+            $responseArray = json_decode($response->getBody()->getContents(), true);
+
+            if (empty($responseArray["features"])) {
+                echo "City name for coordinates {$latitude},{$longitude} was not found." . PHP_EOL;
+                return null;
+            }
+
+            $cityName = $responseArray["features"][0]["text"];
+            $this->updateCache('coordinates', "{$latitude},{$longitude}", $cityName);
+
+            return $cityName;
+        } catch (GuzzleException) {
+            echo "City name for coordinates {$latitude},{$longitude} was not fetched." . PHP_EOL;
+            return null;
+        }
+    }
+
+    protected function updateCache(string $type, string $key, mixed $value): void
+    {
+        $this->cache[$type][$key] = $value;
+        file_put_contents(self::CACHE_FILENAMES[$type], json_encode($this->cache[$type], JSON_UNESCAPED_UNICODE));
+    }
+
+    protected function getFromCacheOrCreate(string $type, string $key, callable $creator): mixed
+    {
+        if (array_key_exists($key, $this->cache[$type])) {
+            return $this->cache[$type][$key];
         }
 
-        file_put_contents(static::CACHE_FILENAME, json_encode($this->cache, JSON_UNESCAPED_UNICODE));
+        $value = $creator();
+
+        if ($value !== null) {
+            $this->updateCache($type, $key, $value);
+        }
+
+        return $value;
     }
 }
+
